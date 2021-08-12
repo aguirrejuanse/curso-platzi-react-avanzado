6cbd36a54846bc28d17919bdbd2c5ad85ba07f08
aguirrejuanse <you@example.com>
2021-08-12 12:54:47 -0300

    add /src and deploy in vercel

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>'Seguimos aca'</h1>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
