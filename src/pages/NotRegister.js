2f27097b8346498b32695b935970535fa5508af7
aguirrejuanse <juanseagui@gmail.com>
2022-01-10 16:06:53 -0300

    created LoginMutation and added in NotRegister component

--- src/pages/NotRegister.js
+++ src/pages/NotRegister.js
@@ -1,34 +1,58 @@
 import React, { useContext } from "react";
 
 import UserForm from "../components/UserForm";
+import { useLoginMutation } from "../containers/LoginMutation";
 import { useRegisterMutation } from "../containers/RegisterMutation";
 import { AppContext } from "../Context";
 
 const NotRegister = () => {
   const { activateAuth } = useContext(AppContext);
-  const { registerMutation, error, data, loading } = useRegisterMutation();
+  const {
+    registerMutation,
+    error: errorRegister,
+    loading: loadingRegister,
+  } = useRegisterMutation();
+  const {
+    loginMutation,
+    error: errorLogin,
+    loading: loadingLogin,
+  } = useLoginMutation();
 
-  const onSubmit = ({ email, password }) => {
+  const onSubmitRegister = ({ email, password }) => {
     const input = { email, password };
     const variable = { input };
-    registerMutation({ variables: variable }).then(() => activateAuth());
+    registerMutation({ variables: variable })
+      .then(() => activateAuth())
+      .catch((err) => console.log(err));
   };
 
-  const errorMsg = error && "El usuario ya existe o hay algún problema";
+  const onSubmitLogin = ({ email, password }) => {
+    const input = { email, password };
+    const variables = { input };
+    loginMutation({ variables })
+      .then(() => activateAuth())
+      .catch((err) => console.log(err));
+  };
+
+  const errorMsgRegister =
+    errorRegister && "El usuario ya existe o hay algún problema";
+
+  const errorMsgLogin =
+    errorLogin && "El usuario no existe o hay algun problema";
 
   return (
     <>
       <UserForm
-        disabled={loading}
-        error={errorMsg}
+        disabled={loadingRegister}
+        error={errorMsgRegister}
         title="Registrarse"
-        onSubmit={onSubmit}
+        onSubmit={onSubmitRegister}
       />
       <UserForm
-        disabled={loading}
-        error={errorMsg}
+        disabled={loadingLogin}
+        error={errorMsgLogin}
         title="Iniciar sesión"
-        onSubmit={activateAuth}
+        onSubmit={onSubmitLogin}
       />
     </>
   );
