3a047e92102f89ddc85463c02b98f69838ba0112
aguirrejuanse <juanseagui@gmail.com>
2022-01-10 14:28:16 -0300

    added useRegisterMutation and created onSubmit

--- src/pages/NotRegister.js
+++ src/pages/NotRegister.js
@@ -1,23 +1,23 @@
 import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
 
 import UserForm from "../components/UserForm";
+import { useRegisterMutation } from "../containers/RegisterMutation";
 import { AppContext } from "../Context";
 
 const NotRegister = () => {
-  const history = useHistory();
-  const { login } = useContext(AppContext);
+  const { activateAuth } = useContext(AppContext);
+  const { registerMutation } = useRegisterMutation();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login(e);
-    history.push("/user");
+  const onSubmit = ({ email, password }) => {
+    const input = { email, password };
+    const variable = { input };
+    registerMutation({ variables: variable }).then(() => activateAuth());
   };
 
   return (
     <>
-      <UserForm title="Registrarse" onSubmit={handleSubmit} />
-      <UserForm title="Iniciar sesión" onSubmit={handleSubmit} />
+      <UserForm title="Registrarse" onSubmit={onSubmit} />
+      <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
     </>
   );
 };
