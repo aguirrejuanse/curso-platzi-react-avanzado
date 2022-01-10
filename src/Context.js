5d5323c4d0682c8eb82be4342627098695885c13
aguirrejuanse <juanseagui@gmail.com>
2022-01-10 16:39:27 -0300

    added token to activeAuth

--- src/Context.js
+++ src/Context.js
@@ -3,10 +3,13 @@ import React, { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(() => {
+    return window.sessionStorage.getItem("token");
+  });
   const user = { isAuth };
-  const activateAuth = () => {
+  const activateAuth = (token) => {
     setIsAuth(true);
+    window.sessionStorage.setItem("token", token);
   };
 
   return (
