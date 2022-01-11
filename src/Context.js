ecb448e780d108c87b32a51199007aef602b0aec
aguirrejuanse <juanseagui@gmail.com>
2022-01-11 09:28:28 -0300

    created removeAuth and added in User.js

--- src/Context.js
+++ src/Context.js
@@ -1,3 +1,4 @@
+import { useApolloClient } from "@apollo/client";
 import React, { createContext, useState } from "react";
 
 export const AppContext = createContext();
@@ -7,13 +8,21 @@ export const AppProvider = ({ children }) => {
     return window.sessionStorage.getItem("token");
   });
   const user = { isAuth };
+
+  const client = useApolloClient();
+
   const activateAuth = (token) => {
     setIsAuth(true);
     window.sessionStorage.setItem("token", token);
   };
 
+  const removeAuth = () => {
+    setIsAuth(false);
+    window.sessionStorage.removeItem("token");
+    client.resetStore();
+  };
   return (
-    <AppContext.Provider value={{ user, activateAuth }}>
+    <AppContext.Provider value={{ user, activateAuth, removeAuth }}>
       {children}
     </AppContext.Provider>
   );
