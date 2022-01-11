434a8f067f0dd2407f8b5d9bb92f71a7b52073ba
aguirrejuanse <juanseagui@gmail.com>
2022-01-11 14:54:05 -0300

    installed webpack-pwa-manifest and created src/assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,64 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const path = require("path");
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: ["@babel/polyfill", "./src/index.js"],
   output: {
-    filename: 'app.bundle.js',
-    publicPath: '/'
+    filename: "app.bundle.js",
+    publicPath: "/",
   },
   devtool: "source-map",
-  mode: 'development',
+  mode: "development",
   devServer: {
     historyApiFallback: {
-      disableDotRule: true
+      disableDotRule: true,
     },
-    liveReload: true
+    liveReload: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
+      template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Petgram - Tu app social de mascotas",
+      short_name: "Petgram 🐶",
+      description:
+        "Con Petgram puedes encontrar y subir fotos de animales domésticos.",
+      background_color: "#ffffff",
+      theme_color: "#2196f3",
+      crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve("src/assets/img/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve("src/assets/img/icon.png"),
+          size: "1024x1024", // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve("src/assets/img/icon.png"),
+          size: "1024x1024",
+          purpose: "maskable",
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.(graphql|gql)$/,
         exclude: /node_modules/,
-        loader: 'graphql-tag/loader'
+        loader: "graphql-tag/loader",
       },
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
-}
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
