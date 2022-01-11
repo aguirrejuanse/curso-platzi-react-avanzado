ef10675e5c82b9112a5da0c6b46ebc5ec1c994ab
aguirrejuanse <juanseagui@gmail.com>
2022-01-11 11:58:41 -0300

    added React.lazy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React, { useContext } from "react";
+import React, { Suspense, useContext } from "react";
 import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 
 import { Logo } from "./components/Logo";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
-import Favs from "./pages/Favs";
 import User from "./pages/User";
 import NotFound from "./pages/NotFound";
 import NotRegister from "./pages/NotRegister";
 import NavBar from "./components/NavBar";
 import { AppContext } from "./Context";
 
+const Favs = React.lazy(() => import("./pages/Favs"));
+
 export const App = () => {
   const {
     user: { isAuth },
   } = useContext(AppContext);
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Logo />
-      <Switch>
-        <Route path="/detail/:detailId" component={Detail} />
-        <Route path="/pet/:id" component={Home} />
-        {!isAuth && <Route path="/login" component={NotRegister} />}
-        {!isAuth && <Redirect from="/favs" to="/login" />}
-        {!isAuth && <Redirect from="/user" to="/login" />}
-        {isAuth && <Redirect from="/login" to="/" />}
-        <Route path="/favs" component={Favs} />
-        <Route path="/user" component={User} />
-        <Route exact path="/" component={Home} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-      <NavBar />
-    </BrowserRouter>
+    <Suspense fallback={<div />}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Logo />
+        <Switch>
+          <Route path="/detail/:detailId" component={Detail} />
+          <Route path="/pet/:id" component={Home} />
+          {!isAuth && <Route path="/login" component={NotRegister} />}
+          {!isAuth && <Redirect from="/favs" to="/login" />}
+          {!isAuth && <Redirect from="/user" to="/login" />}
+          {isAuth && <Redirect from="/login" to="/" />}
+          <Route path="/favs" component={Favs} />
+          <Route path="/user" component={User} />
+          <Route exact path="/" component={Home} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+        <NavBar />
+      </BrowserRouter>
+    </Suspense>
   );
 };
