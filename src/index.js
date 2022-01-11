66c13ef49524855aabc9b49aa5d02f3e56413aeb
aguirrejuanse <juanseagui@gmail.com>
2022-01-11 07:56:03 -0300

    added authMiddelware and errorMiddleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  createHttpLink,
+  from,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 import { App } from "./App";
 import { AppProvider } from "./Context";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "https://petgram-server-aguirrejuanse.vercel.app/graphql",
+});
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = window.sessionStorage.getItem("token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem("token");
+
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  return forward(operation);
+});
+
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === "invalid token") {
+    sessionStorage.removeItem("token");
+    window.location = "/user";
+  }
+});
+
+const client = new ApolloClient({
+  link: from([errorMiddleware, authMiddleware, httpLink]),
   cache: new InMemoryCache(),
 });
 
